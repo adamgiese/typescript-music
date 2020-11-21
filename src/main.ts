5acf387b191348dad6345586a6080ec75aef7766
Adam Giese <adam.giese@underarmour.com>
2020-11-20 22:30:32 -0600

    Add controls for Tone Transport

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-console.log('Γειά σου Κόσμε')
+import * as Tone from 'tone'
+
+const synth = new Tone.Synth().toDestination()
+
+Tone.Transport.scheduleRepeat((time) => {
+  synth.triggerAttackRelease("C4", "8n")
+}, "4n")
+
+// setup the buttons
+
+document.querySelector('#play').addEventListener('click', () => {
+  Tone.Transport.start()
+})
+
+document.querySelector('#pause').addEventListener('click', () => {
+  Tone.Transport.pause()
+})
+
+document.querySelector('#stop').addEventListener('click', () => {
+  Tone.Transport.pause()
+})
